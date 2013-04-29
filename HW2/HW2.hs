fe72def2a7f75a5892602dfcd279770637d7c8e1
Conway <tangco@onid.orst.edu>
2013-04-29 16:49:00 -0700

    finished exercise 1 for HW2

--- HW2/HW2.hs
+++ HW2/HW2.hs
@@ -4,6 +4,10 @@
 
 module HW2 where
 
+import SVG (ppLines)
+import Data.Maybe
+import Data.List
+import System.IO
 
 -- Exercise 1
 type Prog = [Cmd]
@@ -16,41 +20,88 @@ data Cmd = LD Int
 
 type Stack = [Int]
 
-type D = Stack -> Stack
+--type D = Stack -> Stack
+type D = Maybe Stack -> Maybe Stack
 
--- sem :: Prog -> D
--- sem :: Prog -> Stack -> Stack
--- sem plist xs = 
+sem :: Prog -> D
+sem [] a = a
+sem (o:os) b = sem os (semCmd o b)
 
 semCmd :: Cmd -> D
--- SemCmd :: Cmd -> Stack -> Stack
-semCmd (LD i) xs = i:xs
-semCmd ADD xs = (head (take 2 xs) + last (take 2 xs)):(drop 2 xs)
-semCmd MULT xs = (head (take 2 xs) * last (take 2 xs)):(drop 2 xs)
-semCmd DUP xs = (head xs):xs
+semCmd (LD e) xs =
+       case xs of
+            Just xs -> if e < 0
+                 then Nothing
+                 else Just (xs ++ [e])
+semCmd ADD xs = 
+       case xs of
+            Just xs -> if length xs < 2
+                 then Nothing
+                 else Just (init(init xs) ++ [last xs + last(init xs)])
+semCmd MULT xs =
+       case xs of
+            Just xs -> if length xs < 2
+                 then Nothing
+                 else Just (init(init xs) ++ [last xs * last(init xs)])
+semCmd DUP xs =
+       case xs of
+            Just xs -> if length xs < 1
+                 then Nothing
+                 else Just (xs ++ [(last xs)])            
 
-prog1 = [LD 3, DUP, ADD, DUP, MULT]
-prog2 = [LD 3, ADD]
-prog3 = []
+-- stacks
+s1 = Just []
+s2 = Just [1,2,3,4]
+
+-- programs
+p1 = [LD 3,DUP,ADD,DUP,MULT]
+p2 = [LD 3,ADD]
+p3 = []
+
+-- tests
+t1 = sem p1 s1 --should pass
+t2 = sem p2 s1 --should fail
+t3 = sem p1 s2
 
 
 -- Exercise 2
+-- (a)
+data Cmd2 = LD2 Int
+          | ADD2
+          | MULT2
+          | DUP2
+          deriving Show
+
+-- (b)
+type Macros = [(String,Prog)]
+
+-- (c)
+-- sem2 :: Prog -> D
+
+
+-- semCmd2 :: Cmd -> D
+
 
 
 -- Exercise 3
-data Lcmd = Pen Mode
+data Cmd3 = Pen Mode
           | MoveTo Int Int
-          | Seq Lcmd Lcmd
+          | Seq Cmd3 Cmd3
           deriving Show
 
 data Mode = Up | Down
           deriving Show
 
-type State = (Mode, Int, Int)
-type Line = (Int, Int, Int, Int)
+type State = (Mode,Int,Int)
+type Line = (Int,Int,Int,Int)
 type Lines = [Line]
 
--- semS :: Lcmd -> State -> (State, Lines)
-
+semS :: Cmd3 -> State -> (State,Lines)
+semS (Pen Up) (Up,e,f) = ((Up,e,f),[])
+semS (Pen Down) (Up,e,f) = ((Down,e,f),[])
+semS (Pen Down) (Down,e,f) = ((Down,e,f),[])
+semS (Pen Up) (Down,e,f) = ((Down,e,f),[])
+semS (MoveTo e f) (Down,g,h) = ((Down,e,f),(g,h,e,f):[])
+semS (MoveTo e f) (Up,g,h) = ((Up,e,f),[])
 
 -- sem' :: Lcmd -> Lines
